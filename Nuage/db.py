cd593121cb75cb181e652f3db5cc96c4d7a36dad
yacine20005 <yacine200005@gmail.com>
2024-11-25 22:05:14 +0100

    Mise à jour de la connexion à la base de données avec de nouveaux paramètres pour l'utilisateur et le mot de passe, et changement d'hôte vers localhost.

--- Nuage/db.py
+++ Nuage/db.py
@@ -3,10 +3,12 @@ import psycopg2.extras
 
 def connect():
     conn = psycopg2.connect(
-        host = 'sqledu.univ-eiffel.fr',
-        dbname = 'zelia_db', # nom de votre base de données
-        password = 'miaou18', # mot de passe de la base
-        cursor_factory = psycopg2.extras.NamedTupleCursor,
+        dbname='dump-nuage-HAMADOUCHE-GAVAU--PELISSIER',
+        user='votre_nom_utilisateur',  # Remplacez par votre nom d'utilisateur PostgreSQL
+        password='',  # Remplacez par votre mot de passe PostgreSQL
+        host='localhost',
+        port='5432',
+        cursor_factory=psycopg2.extras.NamedTupleCursor,
     )
     conn.autocommit = True
     return conn
